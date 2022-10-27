821cc3cbe8b8a259d402e660d2c062fcf3b88299
Paul2D <paul.maxineanu@gmail.com>
2022-10-27 17:44:11 +0300

    create svelte components and dispatch events

---
+++ src/components/RatingList.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    import RatingItem from "./RatingItem.svelte";
+    export let ratingList = []
+
+    const dispatch = createEventDispatcher();
+
+    function forward (e) {
+        dispatch('delete-rating', e.detail)
+    } 
+</script>
+
+{#each ratingList as rating (rating.id)}
+    <RatingItem {rating} on:delete-rating={forward} />
+{/each}
